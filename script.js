bab2fc590f8d6813d990907e4c3257e54b19fc3e
Y-Jacob Mlo <y-jacobmlo@AdminisatorsMBP.lan>
2022-08-30 10:46:16 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /* OPEN NAV */
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
-
 }
 /* Close Nav */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0px";
 }
-
